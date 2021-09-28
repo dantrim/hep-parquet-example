8ccbf8ac6e46ad9aa9924d1e28ac8248f64e9dc5
dantrim <dantrim1023@gmail.com>
2021-09-27 17:40:51 -0700

    update parquet writer

--- src/event_generator.h
+++ src/event_generator.h
@@ -7,148 +7,146 @@
 #include <random>
 
 struct Event {
-    double w;
-    double w2;
-    std::vector<bool> trigger_mask;
+  double w;
+  double w2;
+  std::vector<bool> trigger_mask;
 };
 
 class EventGen {
-    public :
-        EventGen();
-        ~EventGen() = default;
+public:
+  EventGen();
+  ~EventGen() = default;
 
-        double w();
-        std::vector<bool> trigger_mask();
+  double w();
+  std::vector<bool> trigger_mask();
 
-    private :
-        std::default_random_engine _rng;
-        std::normal_distribution<double> _w_dist;
-        std::uniform_real_distribution<float> _trig_dist;
+private:
+  std::default_random_engine _rng;
+  std::normal_distribution<double> _w_dist;
+  std::uniform_real_distribution<float> _trig_dist;
 };
 
 struct Lepton {
-    float pt;
-    float eta;
-    float phi;
-    unsigned flavor;
-    std::vector<bool> trigger_match_mask;
-    bool isLoose;
-    bool isMedium;
-    bool isTight;
+  float pt;
+  float eta;
+  float phi;
+  unsigned flavor;
+  std::vector<bool> trigger_match_mask;
+  bool isLoose;
+  bool isMedium;
+  bool isTight;
 };
 
 class LeptonGen {
-    public :
-        LeptonGen(float max_pt);
-        ~LeptonGen() = default;
+public:
+  LeptonGen(float max_pt);
+  ~LeptonGen() = default;
 
-        int nLeptons();
-        float pt();
-        float eta();
-        float phi();
-        unsigned flavor();
-        std::vector<bool> trigger_match_mask();
-        bool isLoose();
-        bool isMedium();
-        bool isTight();
+  int nLeptons();
+  float pt();
+  float eta();
+  float phi();
+  unsigned flavor();
+  std::vector<bool> trigger_match_mask();
+  bool isLoose();
+  bool isMedium();
+  bool isTight();
 
-    private :
-        float _max_pt;
+private:
+  float _max_pt;
 
-        std::default_random_engine _rng;
-        std::uniform_int_distribution<int> _lep_eff_dist;
-        std::uniform_real_distribution<float> _pt_dist;
-        std::uniform_real_distribution<float> _eta_dist;
-        std::uniform_real_distribution<float> _phi_dist;
-        std::uniform_real_distribution<float> _trig_dist;
+  std::default_random_engine _rng;
+  std::uniform_int_distribution<int> _lep_eff_dist;
+  std::uniform_real_distribution<float> _pt_dist;
+  std::uniform_real_distribution<float> _eta_dist;
+  std::uniform_real_distribution<float> _phi_dist;
+  std::uniform_real_distribution<float> _trig_dist;
 };
 
 struct Jet {
-    float pt;
-    float eta;
-    float phi;
-    float mass;
-    float truthHadronPt;
-    unsigned truthHadronId;
-    unsigned nTrk;
-    bool isBJet;
-    float bTagScore;
+  float pt;
+  float eta;
+  float phi;
+  float mass;
+  float truthHadronPt;
+  unsigned truthHadronId;
+  unsigned nTrk;
+  bool isBJet;
+  float bTagScore;
 };
 
 class JetGen {
-    public :
-        JetGen(float max_pt, int max_n);
-        ~JetGen() = default;
+public:
+  JetGen(float max_pt, int max_n);
+  ~JetGen() = default;
 
-        int nJets();
-        float pt();
-        float eta();
-        float phi();
-        float mass();
-        unsigned truthHadronId();
-        unsigned nTrk();
-        float bTagScore();
+  int nJets();
+  float pt();
+  float eta();
+  float phi();
+  float mass();
+  unsigned truthHadronId();
+  unsigned nTrk();
+  float bTagScore();
 
-    private :
-        float _max_pt;
-        int _max_n;
+private:
+  float _max_pt;
+  int _max_n;
 
-        std::default_random_engine _rng;
-        std::uniform_int_distribution<int> _jet_eff_dist;
-        std::uniform_real_distribution<float> _pt_dist;
-        std::uniform_real_distribution<float> _eta_dist;
-        std::uniform_real_distribution<float> _phi_dist;
-        std::uniform_real_distribution<float> _trig_dist;
-        std::uniform_int_distribution<int> _jet_ntrk_dist;
-
+  std::default_random_engine _rng;
+  std::uniform_int_distribution<int> _jet_eff_dist;
+  std::uniform_real_distribution<float> _pt_dist;
+  std::uniform_real_distribution<float> _eta_dist;
+  std::uniform_real_distribution<float> _phi_dist;
+  std::uniform_real_distribution<float> _trig_dist;
+  std::uniform_int_distribution<int> _jet_ntrk_dist;
 };
 
 struct Met {
-    float sumEt;
-    float met;
-    float phi;
-    float electronTerm;
-    float muonTerm;
-    float jetTerm;
-    float softTerm;
+  float sumEt;
+  float met;
+  float phi;
+  float electronTerm;
+  float muonTerm;
+  float jetTerm;
+  float softTerm;
 };
 
 class MetGen {
-    public :
-        MetGen(float max_pt);
-        ~MetGen() = default;
+public:
+  MetGen(float max_pt);
+  ~MetGen() = default;
 
-        float sumEt();
-        float met();
-        float phi();
-    private :
-        float _max_pt;
-        std::default_random_engine _rng;
-        std::uniform_real_distribution<float> _pt_dist;
-        std::uniform_real_distribution<float> _phi_dist;
+  float sumEt();
+  float met();
+  float phi();
+
+private:
+  float _max_pt;
+  std::default_random_engine _rng;
+  std::uniform_real_distribution<float> _pt_dist;
+  std::uniform_real_distribution<float> _phi_dist;
 };
 
 class EventGenerator {
-    public :
-        EventGenerator(float max_pt, unsigned max_lep, unsigned max_jet);
-        ~EventGenerator() = default;
+public:
+  EventGenerator(float max_pt, unsigned max_lep, unsigned max_jet);
+  ~EventGenerator() = default;
 
-        Event event();
-        std::vector<Lepton> leptons();
-        std::vector<Jet> jets();
-        Met met();
+  Event event();
+  std::vector<Lepton> leptons();
+  std::vector<Jet> jets();
+  Met met();
 
-    private :
+private:
+  std::shared_ptr<EventGen> _event_gen;
+  std::shared_ptr<LeptonGen> _lepton_gen;
+  std::shared_ptr<JetGen> _jet_gen;
+  std::shared_ptr<MetGen> _met_gen;
 
-        std::shared_ptr<EventGen> _event_gen;
-        std::shared_ptr<LeptonGen> _lepton_gen;
-        std::shared_ptr<JetGen> _jet_gen;
-        std::shared_ptr<MetGen> _met_gen;
-
-        unsigned _max_nleptons;
-        unsigned _max_njets;
-        float _max_pt;
+  unsigned _max_nleptons;
+  unsigned _max_njets;
+  float _max_pt;
 };
-
 
 #endif
--- src/tools/gen_dataset.cpp
+++ src/tools/gen_dataset.cpp
@@ -1,122 +1,145 @@
-#include <iostream>
 #include "event_generator.h"
+#include "logging.h"
 #include "parquet_writer.h"
 #include <fstream>
-#include "logging.h"
+#include <iostream>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
 
-    if(argc != 3) {
-        std::cerr << "Incorrect number of arguments" << std::endl;
-        std::cerr << "Usage: " << argv[0] << " <n-events> <config>" << std::endl;
-        return 1;
+  if (argc != 3) {
+    std::cerr << "Incorrect number of arguments" << std::endl;
+    std::cerr << "Usage: " << argv[0] << " <n-events> <config>" << std::endl;
+    return 1;
+  }
+  unsigned n_events = std::stoi(argv[1]);
+  std::cout << "Generating " << n_events << " events" << std::endl;
+
+  // initialize parquet-writer
+  std::ifstream ifs(argv[2]);
+  namespace pw = parquetwriter;
+  // pw::logging::set_debug();
+  pw::Writer writer;
+  writer.set_layout(ifs);
+  writer.set_metadata(ifs);
+  writer.set_dataset_name("hep_dataset");
+  writer.set_output_directory("hep_dataset");
+  writer.set_compression(pw::Compression::UNCOMPRESSED);
+  writer.initialize();
+
+  EventGenerator gen(100., 3, 10);
+
+  for (size_t ievent = 0; ievent < n_events; ievent++) {
+
+    if (ievent % 100 == 0 || (ievent + 1) == n_events) {
+      std::cout << "====================================================="
+                << std::endl;
+      std::cout << " *** Processing event "
+                << (ievent % 100 == 0 ? ievent : (ievent + 1)) << "/"
+                << n_events << " ***" << std::endl;
     }
-    unsigned n_events = std::stoi(argv[1]);
-    std::cout << "Generating " << n_events << " events" << std::endl;
 
-    // initialize parquet-writer
-    std::ifstream ifs(argv[2]);
-    namespace pw = parquetwriter;
-    //pw::logging::set_debug();
-    pw::Writer writer;
-    writer.set_layout(ifs);
-    writer.set_metadata(ifs);
-    writer.set_dataset_name("hep_dataset");
-    writer.set_output_directory("hep_dataset");
-    writer.set_compression(pw::Compression::UNCOMPRESSED);
-    writer.initialize();
+    auto event = gen.event();
+    auto leptons = gen.leptons();
+    auto jets = gen.jets();
+    auto met = gen.met();
 
-    EventGenerator gen(100., 3, 10);
+    //
+    // event container
+    //
+    uint32_t event_idx = static_cast<uint32_t>(ievent);
+    double w = event.w;
+    double w2 = event.w2;
+    std::vector<bool> trigger_mask = event.trigger_mask;
+    pw::struct_t event_container_data{
+        {"index", event_idx},
+        {"w", w},
+        {"w2", w2},
+        {"trigger_mask", trigger_mask}
+    };
 
-    for(size_t ievent = 0; ievent < n_events; ievent++) {
+    //
+    // leptons container
+    //
+    uint32_t nleptons = leptons.size();
+    pw::struct_t lepton_container_data{
+        {"size", nleptons}
+    };
 
-        if(ievent % 100 == 0 || (ievent+1) == n_events) {
-        std::cout << "=====================================================" << std::endl;
-            std::cout << " *** Processing event " << (ievent % 100 == 0 ? ievent : (ievent+1)) << "/" << n_events << " ***" << std::endl;
-        }
-
-        auto event = gen.event();
-        auto leptons = gen.leptons();
-        auto jets = gen.jets();
-        auto met = gen.met();
-
-        //
-        // event container
-        //
-        uint32_t event_idx = static_cast<uint32_t>(ievent);
-        double w = event.w;
-        double w2 = event.w2;
-        std::vector<bool> trigger_mask = event.trigger_mask;
-        pw::struct_t event_container_data{event_idx, w, w2, trigger_mask};
-
-        //
-        // leptons container
-        //
-        uint32_t nleptons = leptons.size();
-        pw::struct_t leptons_container_data{nleptons};
-
-        pw::struct_list1d leptons_data;
-        for(size_t ilepton = 0; ilepton < leptons.size(); ilepton++) {
-            Lepton lepton = leptons.at(ilepton);
-            float pt = lepton.pt;
-            pw::struct_t lepton_data{
-                                    pt,
-                                    lepton.eta,
-                                    lepton.phi,
-                                    lepton.flavor,
-                                    lepton.trigger_match_mask,
-                                    lepton.isLoose,
-                                    lepton.isMedium,
-                                    lepton.isTight};
-            leptons_data.push_back(lepton_data);
-        }
-
-        //
-        // jets container
-        //
-        uint32_t njets = jets.size();
-        pw::struct_t jets_struct{njets};
-        pw::struct_list1d jets_container_data;
-        for(size_t i = 0; i < njets; i++) {
-            auto jet = jets.at(i);
-            pw::struct_t jet_data{
-                jet.pt,
-                    jet.eta,
-                    jet.phi,
-                    jet.mass,
-                    jet.truthHadronPt,
-                    jet.truthHadronId,
-                    jet.nTrk,
-                    jet.isBJet,
-                    jet.bTagScore};
-            jets_container_data.push_back(jet_data);
-
-        }
-
-        // 
-        // met container
-        //
-        pw::struct_t met_data{met.sumEt,
-                                met.met,
-                                met.phi,
-                                met.electronTerm,
-                                met.muonTerm,
-                                met.jetTerm,
-                                met.softTerm};
-
-
-        // fill columns
-        writer.fill("event_container", {event_container_data});
-        writer.fill("lepton_container", {leptons_container_data});
-        writer.fill("lepton_container.leptons", {leptons_data});
-        writer.fill("jet_container", {jets_struct});
-        writer.fill("jet_container.jets", {jets_container_data});
-        writer.fill("met_container", {met_data});
-
-        // finish this row
-        writer.end_row();
+    std::vector<pw::struct_t> leptons_data;
+    for (size_t ilepton = 0; ilepton < leptons.size(); ilepton++) {
+      Lepton lepton = leptons.at(ilepton);
+      pw::struct_t lepton_data{
+          {"pt", lepton.pt},
+          {"eta", lepton.eta},
+          {"phi", lepton.phi},
+          {"flavor", lepton.flavor},
+          {"trigger_match_mask", lepton.trigger_match_mask},
+          {"isLoose", lepton.isLoose},
+          {"isMedium", lepton.isMedium},
+          {"isTight", lepton.isTight}
+      };
+      leptons_data.push_back(lepton_data);
     }
-    writer.finish();
 
-    return 0;
+    //
+    // jets container
+    //
+    uint32_t njets = jets.size();
+    pw::struct_t jets_container_data{
+        {"size", njets}
+    };
+    std::vector<pw::struct_t> jets_data;
+    for (size_t i = 0; i < njets; i++) {
+      auto jet = jets.at(i);
+      pw::struct_t jet_data{
+          {"pt", jet.pt},
+          {"eta", jet.eta},
+          {"phi", jet.phi},
+          {"mass", jet.mass},
+          {"truthHadronPt", jet.truthHadronPt},
+          {"truthHadronId", jet.truthHadronId},
+          {"nTrk", jet.nTrk},
+          {"isBJet", jet.isBJet},
+          {"bTagScore", jet.bTagScore}
+      };
+      jets_data.push_back(jet_data);
+    }
+
+    //
+    // met container
+    //
+    pw::struct_t met_data{
+        {"sumEt", met.sumEt},
+        {"met", met.met},
+        {"phi", met.phi},
+        {"electronTerm", met.electronTerm},
+        {"muonTerm", met.muonTerm},
+        {"jetTerm", met.jetTerm},
+        {"softTerm", met.softTerm}
+    };
+
+    //
+    // fill columns
+    //
+
+    // event container
+    writer.fill_struct("event_container", {event_container_data});
+
+    // lepton container
+    writer.fill_struct("lepton_container", {lepton_container_data});
+    writer.fill_struct_list("lepton_container.leptons", {leptons_data});
+
+    // jet container
+    writer.fill_struct("jet_container", {jets_container_data});
+    writer.fill_struct_list("jet_container.jets", {jets_data});
+
+    // met container
+    writer.fill_struct("met_container", {met_data});
+
+    // finish this row
+    writer.end_row();
+  }
+  writer.finish();
+
+  return 0;
 }
